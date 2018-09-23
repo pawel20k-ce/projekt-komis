5b1e0b3375f7c56cd6c4065bef91d6e09a229ade
Pawel <pawel20k-ce@wp.pl>
2018-09-23 10:51:08 +0200

    Initial commit

---
+++ src/main/java/pl/ottenburger/projekt/KomisApplication.java
@@ -0,0 +1,16 @@
+package pl.ottenburger.projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KomisApplication.class, args);
+    }
+
+
+
+
+}
